fb67e06d5590f03b27ff7a1405bf9e1d587f0bcc
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2026-06-04 15:28:12 +0200

    Added paintComponent-based board

--- src/main/chess/MainWindow.java
+++ src/main/chess/MainWindow.java
@@ -5,8 +5,8 @@ import javax.swing.*;
 public class MainWindow extends JFrame {
     public MainWindow() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        add(new GamePanelComponents(60));
-        //add(new GamePanelDraw(60));
+        //add(new GamePanelComponents(60));
+        add(new GamePanelDraw(60));
         setVisible(true);
     }
 }
